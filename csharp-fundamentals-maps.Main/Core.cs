df19496aedfba901791c95a918c9f06aa68b7d1e
Hans Eivind Johnsen Skinstad <Hans.Eivind.Skinstad@no.experis.com>
2025-01-08 11:40:09 +0100

    All tests pass. Exercise finished!

--- csharp-fundamentals-maps.Main/Core.cs
+++ csharp-fundamentals-maps.Main/Core.cs
@@ -48,9 +48,9 @@ namespace csharp_fundamentals_maps.Main
 
         public string getValue(string key)
         {
-            
+
            
-            return string.Empty;
+            return createPerson()[key];
 
 
         }
@@ -64,7 +64,7 @@ namespace csharp_fundamentals_maps.Main
          */
          public bool hasKey(Dictionary<string,string> dictionary, string isitthere)
          {
-            return false;
+            return dictionary.ContainsKey(isitthere);
             
          }
 
@@ -78,7 +78,7 @@ namespace csharp_fundamentals_maps.Main
          */
         public int getValueOrDefault(Dictionary<string,int> dictionary, string isitthere)
         {
-            return 0;
+            return dictionary.GetValueOrDefault(isitthere);
 
         }
 
@@ -104,8 +104,9 @@ namespace csharp_fundamentals_maps.Main
             map.Add(7, "muse");
             map.Add(96, "nice");
             // Write your code below this comment...
-
-           
+            foreach(var item in numbers)
+                if(map.ContainsKey(item))
+                    results.Add(map[item]);
 
             //    // ...and above this comment
             return results;
